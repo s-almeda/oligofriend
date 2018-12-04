af4576f1867d9e541e512b73ee1796bcfa46a391
Sarah Almeda <almedas1@tcnj.edu>
2018-12-04 03:23:00 -0500

    update Procfile and pom.xml

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import hello.HelloServlet;
-public class Main {
-
-   public static void main(String[] args) {
-       HelloServlet hello = new HelloServlet();
-       System.out.println("Done!");
-   }
-}
--- src/main/java/hello/HelloServlet.java
+++
@@ -1,8 +0,0 @@
-package hello;
-
-public class HelloServlet {
-    public static void main(String[] args) {
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
-    }
-}
