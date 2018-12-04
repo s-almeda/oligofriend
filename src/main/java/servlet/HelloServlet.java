7dcff31b9b8b24fa5b9ccc3ced1018a1a641a062
Sarah Almeda <almedas1@tcnj.edu>
2018-12-04 03:33:13 -0500

    Java integration test

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -20,7 +20,7 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
-        out.write("hello heroku<br>".getBytes());
+        out.write("hello heroku\n".getBytes());
         Greeter greeter = new Greeter();
         out.write(greeter.sayHello().getBytes());
         out.flush();
