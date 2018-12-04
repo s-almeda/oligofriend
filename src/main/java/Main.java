880457ec027eca861d90a4c5e45382cc4111495f
Sarah Almeda <almedas1@tcnj.edu>
2018-12-04 02:35:57 -0500

    created Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import hello.HelloServlet;
+public class Main {
+
+   public static void main(String[] args) {
+       HelloServlet hello = new HelloServlet();
+       System.out.println("Done!");
+   }
+}
