6bf7043ab0595d5450c6275a9f1481a2f79cd451
Sarah Almeda <almedas1@tcnj.edu>
2018-12-09 02:36:39 -0500

    database

--- src/main/java/employees/EmployeeServlet.java
+++ src/main/java/employees/EmployeeServlet.java
@@ -56,7 +56,7 @@ public class EmployeeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         final String UPLOAD_DIRECTORY = "C:/uploads";
-  
+
         if (ServletFileUpload.isMultipartContent(request)) {
 
             try {
